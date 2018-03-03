a773627d739d2abc6d29e01a6e4847e768615abd
Rohit Varkey Thankachan <rohitvarkey@gmail.com>
2018-03-03 13:00:35 -0500

    Summary

--- functions/index.js
+++ functions/index.js
@@ -14,6 +14,7 @@ const NAME_ACTION = 'activity';
 // b. the parameters that are parsed from the make_name intent
 const STEP_ARGUMENT = 'track';
 const WORKOUT_ARGUMENT = 'workout';
+const COUNT_ARGUMENT = 'num';
 const USER = 'rohitvarkey';
 
 
@@ -21,6 +22,7 @@ exports.workout_diary_step = functions.https.onRequest((request, response) => {
   const app = new App({request, response});
   console.log('Request headers: ' + JSON.stringify(request.headers));
   console.log('Request body: ' + JSON.stringify(request.body));
+  console.log(app.getUser());
 
 // c. The function that generates the silly name
   function makeResponse (app) {
@@ -52,9 +54,75 @@ exports.workout_diary_step = functions.https.onRequest((request, response) => {
         ' reps of ' + exercise + '. You can do it!');
     })
   }
+
+  function logCounts (app) {
+    console.log("count invoked");
+    let workout_type = app.getArgument(WORKOUT_ARGUMENT);
+    let step = eval(app.getArgument(STEP_ARGUMENT));
+    let counts = app.getArgument(COUNT_ARGUMENT);
+    let max_count = Math.max(...counts);
+    //let step = 1;
+    if (step == null) {
+        step = 1
+    }
+    if (workout_type == null) {
+        workout_type = 'Monday'
+    }
+    console.log("Invoked with ", workout_type, step, USER, counts);
+    var ref = db.ref("users/" + USER + '/' + workout_type +'/' + step);
+    console.log("users/" + USER + '/' + workout_type +'/' + step);
+    console.log("Ref", ref);
+    ref.on('value', function(snapshot) {
+      console.log("snapshot:", snapshot);
+      console.log("Val", snapshot.val());
+      let val = snapshot.val();
+      if (val == null) {
+        app.tell("Something went wrong.");
+        return;
+      }
+      let reps = val['reps'];
+      let exercise = val['exercise'];
+      let diff = reps - max_count;
+      if (max_count < reps) {
+        app.ask('You have' + diff + 'reps left for' + exercise + '. Would you like to skip or continue?');
+      }
+      else {
+        app.ask('Well done! You completed ' + max_count + ' ' + exercise + '. Say done to start the next exercise.');
+      }
+    })
+
+  }
+
+  function summary(app) {
+    console.log("summary invoked");
+    let workout_type = app.getArgument(WORKOUT_ARGUMENT);
+    if (workout_type == null) {
+        workout_type = 'Monday'
+    }
+    console.log("Invoked with ", workout_type, USER);
+    var ref = db.ref("users/" + USER + '/' + workout_type);
+    console.log("users/" + USER + '/' + workout_type);
+    console.log("Ref", ref);
+    summary_str = "Your " + workout_type + "workout consists of ";
+    ref.on('value', function(snapshot) {
+      console.log("snapshot:", snapshot);
+      console.log("Val", snapshot.val());
+      let val = snapshot.val();
+      if (val == null) {
+        app.tell("Something went wrong.");
+        return;
+      }
+      let reps = val['reps'];
+      let exercise = val['exercise'];
+      summary_str += reps + " " + exercise + ", ";
+    })
+    summary_str += ". Tell me when you want to begin!"
+    app.ask(summary_str);
+  }
   // d. build an action map, which maps intent names to functions
   let actionMap = new Map();
   actionMap.set(NAME_ACTION, makeResponse);
+  actionMap.set('count', logCounts);
 
 
 app.handleRequest(actionMap);
